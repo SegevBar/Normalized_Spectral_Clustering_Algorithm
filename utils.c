35c39821ace3adbf6a4fad6ba7dd7d743fdcb418
Bar Segev <segevbar94@gmail.com>
2022-09-13 05:57:11 +0300

    round

--- utils.c
+++ utils.c
@@ -138,7 +138,7 @@ void printMatrix(double **matrix, int n, int m) {
 
     for (i = 0; i < n; i++) {
         for (j = 0; j < m; j++) {
-            printf("%.4f", round(matrix[i][j]));
+            printf("%.4f", (matrix[i][j]));
             if (j != m-1) {
                 printf(",");
             } else {
