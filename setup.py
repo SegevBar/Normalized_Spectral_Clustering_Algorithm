12859941545fe41ea808220567681e0d3f53b2a4
Bar Segev <segevbar94@gmail.com>
2022-09-13 06:59:48 +0300

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from setuptools import Extension, setup
 module = Extension("spkmeansmodule",
                   sources=[
                     'spkmeans.c',
-                    'spkmeansmodule.c'
+                    'spkmeansmodule.c',
+                    'spkmeans.c',
+                    'wam.c',
+                    'ddg.c',
+                    'lnorm.c',
+                    'jacobi.c'
+                    'utils.c'
+                    'normalizedKEigenvectorsMatrix.c'
                   ])
 setup(name='spkmeansmodule',
      version='1.0',
